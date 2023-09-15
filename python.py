36da12a74528ee7969f0c492061e2767867e780c
ismoil27 <abduraimovismoil98@gmail.com>
2023-09-15 19:21:40 +0900

    feat: guess the number game developed by while and if

--- python.py
+++ python.py
@@ -30,6 +30,7 @@
 x = input('Please enter the number?\n')
 a = int(x)
 print(type(a))
+
 '''
 # ==================
 
@@ -44,9 +45,84 @@ print(type(a))
 # print(total)
 
 # celsius = input('Write temperature in celsius:')
-# print(celsius)
-# convert_into_float = float(celsius)
-# fahrenheit = convert_into_float * 1.8000 + 32.00
-# print("Your entered temperature in fahrenheit is:", fahrenheit)
+# convert_into_float = int(celsius)
+
+# # print(celsius)
+
+# if type(convert_into_float) != int:
+#     celsius = input('Oops! We ask you to enter intiger:')
+# elif type(convert_into_float) == int:
+#     convert_into_float = float(celsius)
+#     fahrenheit = convert_into_float * 1.8000 + 32.00
+#     print("Your entered temperature in fahrenheit is:", fahrenheit)
+# else:
+#     print('Your time is up!')
+
+
+# ====================== Into to Programming. Lesson 3 ===========================
+# Rule 1: Always make 'comment' if your code is nested or 
+
+# print(3 == 3)
+
+# if(17 and True):
+#     print('hello') 
+
+# if(-17 or False):
+#     print('17 is converted to Bool')
+# else:
+#     print('Not converted')
+
+
+# print(bool('hi'))
+
+# x = 7
+
+# if x % 2 == 0:
+#     print('x is even')
+# if x % 2 != 0:
+#     print('x is odd')
+
+
+# inp = input('Enter Fahrenheit Temperature:')
+# try:
+#     fahr = float(inp)
+#     cel = (fahr - 32.0) * 5.0 / 0
+#     print(cel)
+# except Exception as e:
+#     print(e)
+
+
+number_to_guess = 42
+max_attempts = 6
+attempts = 0
+
+print("Welcome to the Guess the Number game!")
+print(f"Guess the number between 0 and 100. You have {max_attempts} attempts.")
+
+
+while attempts < max_attempts:
+    try:
+        guess = int(input("Enter your guess: "))
+    except Exception as e:
+        print("Invalid input. Please enter a number.")
+        continue 
+
+    if guess < 0 or guess > 100:
+        print("Please enter a number between 0 and 100.")
+        continue
+
+    attempts += 1
+
+    if guess < number_to_guess:
+        print("Too low. Try again.")
+    elif guess > number_to_guess:
+        print("Too high. Try again.")
+    else:
+        print(f"Congratulations! You guessed the number {number_to_guess} in {attempts} attempts.")
+        break
+
+else:
+    print(f"Sorry, you've reached the maximum number of attempts. The correct number was {number_to_guess}.")
+
 
 
