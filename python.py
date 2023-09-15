ec9f7ddc1421075bc5e676e8d8f3f5c1583f5107
ismoil27 <abduraimovismoil98@gmail.com>
2023-09-15 17:10:06 +0900

    feat: initial commit

---
+++ python.py
@@ -0,0 +1,52 @@
+# x = 15
+
+# if x < 10:
+#     print('Smaller')
+
+# if x > 20:
+#     print('Bigger')
+
+
+# if x > 10 and x < 20:
+#     print('Average')
+
+
+# print('==================')
+# print('Finished the task')
+
+
+
+# n = 5
+# while n > 0:
+#     print(n)
+#     n = n - 1
+
+# print('Blastoff!')
+
+
+
+
+'''
+x = input('Please enter the number?\n')
+a = int(x)
+print(type(a))
+'''
+# ==================
+
+
+# hours = input('Enter hours?:\n')
+# rate = input('Enter Rate:\n')
+# print(hours)
+# print(rate)
+# convert_hours_to_int = int(hours)
+# convert_rate_to_float = float(rate)
+# total = convert_hours_to_int * convert_rate_to_float
+# print(total)
+
+# celsius = input('Write temperature in celsius:')
+# print(celsius)
+# convert_into_float = float(celsius)
+# fahrenheit = convert_into_float * 1.8000 + 32.00
+# print("Your entered temperature in fahrenheit is:", fahrenheit)
+
+
