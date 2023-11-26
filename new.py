52269700511e5b3d60f83cd3df30e9ee016e5081
ismoil27 <abduraimovismoil98@gmail.com>
2023-11-26 18:40:03 +0900

    feat: conditional operators

--- new.py
+++ new.py
@@ -1,7 +1,27 @@
-print('How many kilometres did you run today?')
-kms = input()
-miles = float(kms) / 1.60934
-miles = round(miles, 2)
-print(f"Your {kms}km ride was {miles}miles ")
+# print('What is your favorite color?')
+# data = input()
+# print('You said ' + data)
 
-# round(thing to round, how many decimal points)
+# name = 'snow'
+
+# if name == 'Arya Stark':
+#     print('You are Arya')
+# elif name == 'jon snow':
+#     print('You are Jon Snow')
+# else:
+#     print('Carry on')
+
+
+# animal = input('What is favorite animal?')
+
+# if animal:
+#     print(animal + ' is my favorite too!')
+# else:
+#     print("YOU DID NOT SAY ANYTHING NIGGA!")
+
+# city = input('Where do you live? \n')
+
+# if city == 'los angeles' or city == 'san francisco' or 'california':
+#     print('YOU LIVE IN CALIFORNIA!')
+# else:
+#     print("You live somewhere else!")
