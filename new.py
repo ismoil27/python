d7b62321f1278cb73461ec01b709c62daace2adf
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-17 21:05:23 +0900

    learning list

--- new.py
+++ new.py
@@ -56,3 +56,97 @@
 #     else:
 #         state = 'odd'
 #     print(f"{numb} is {state}")
+
+
+# msg = input('what is the password?')
+# while msg != 'banana':
+#     print('WRONG!')
+#     msg = input('What is the secret password?')
+# print('CORRECT!')
+
+
+# for num in range(1, 11):
+#     print(num)
+
+# num = 1
+# while num <= 11:
+#     print(num)
+#     num += 1
+# for num in range(1, 11):
+#     print("\U0001f600" * num)
+
+
+# times = 1
+# while times <= 11:
+#     print("\U0001f600" * times)
+#     times += 1
+
+
+# times = 1
+# while times < 10:
+#     spaces = " " * (10 - times)
+#     emojis = "\U0001f600" * times
+#     print(spaces + emojis)
+#     times += 2
+
+# ===================================
+# msg = input('Hey how is it going? ')
+# while msg != 'stop copying me':
+#     print(msg)
+#     msg = input()
+# print('UGH FINE YOU WIN!')
+# ===================================
+
+
+# while True:
+#     command = input("Type 'exit' to exit: ")
+#     if (command == 'exit'):
+#         break
+
+
+# times = int(input('How many times do I have to tell you? '))
+
+# for time in range(times):
+#     print('Clean up your room!')
+#     if time >= 3:
+#         print('do you even listen to me? ')
+#         break
+
+# import random
+# random_number = random.randint(1, 10)
+# guess = None
+
+# while True:
+#     guess = int(input('Pick a number from 1 to 10: '))
+#     if guess < random_number:
+#         print('too low!')
+#     elif guess > random_number:
+#         print('too high!')
+#     else:
+#         print('You won!')
+#         play_again = input('Do you want to play again? (y/n) ')
+#         if play_again == 'y':
+#             random_number = random.randint(1, 10)
+#             guess = None
+#         else:
+#             print('Thank you for playing!')
+#             break
+
+
+# colors = ['purple', 'black', 'gray', 'red', 'white']
+
+# index = 0
+
+# while index < len(colors):
+#     print(f"{index}: {colors[index]}")
+#     index += 1
+# for color in colors:
+#     print(color)
+
+
+numbers = [1, 2, 4]
+print(numbers)
+
+numbers.extend([5, 6, 7])
+
+print(numbers)
--- python.py
+++ python.py
@@ -360,23 +360,23 @@ print(type(a))
 # print('Initialising Hello World Game')
 
 
-import urllib.request
-import urllib.parse
-import urllib.error
-from bs4 import BeautifulSoup
-import ssl
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+# import urllib.request
+# import urllib.parse
+# import urllib.error
+# from bs4 import BeautifulSoup
+# import ssl
+# # Ignore SSL certificate errors
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
+# url = input('Enter - ')
+# html = urllib.request.urlopen(url, context=ctx).read()
+# soup = BeautifulSoup(html, 'html.parser')
+# # Retrieve all of the anchor tags
+# tags = soup('a')
+# for tag in tags:
+#     print(tag.get('href', None))
+#     print('TAG:', tag)
+#     print('URL:', tag.get('href', None))
+#     print('Contents:', tag.contents[0])
+#     print('Attrs:', tag.attrs)
